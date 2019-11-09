2740592da9be5833b35bbe2c0f45f9b8a94fe3f2
biachacon <anabheatrizchacon@outlook.com>
2019-11-09 20:52:34 -0300

    biometric

---
+++ app/src/main/java/com/biachacon/sensorsdemo/BiometricUtils.kt
@@ -0,0 +1,21 @@
+package com.biachacon.sensorsdemo
+
+import android.content.Context
+import android.hardware.biometrics.BiometricManager
+import android.os.Build
+import androidx.core.hardware.fingerprint.FingerprintManagerCompat
+
+fun isBiometricAvailable(context: Context): Boolean {
+    return if (Build.VERSION.SDK_INT < 29) {
+        isHardwareSupported(context) && isFingerprintAvailable(context)
+    } else { //Biometric manager (from Android Q)
+        val biometricManager = context.getSystemService(BiometricManager::class.java)
+        if (biometricManager != null) {
+            biometricManager.canAuthenticate() == BiometricManager.BIOMETRIC_SUCCESS
+        } else false
+    }
+}
+
+private fun isHardwareSupported(context: Context) = FingerprintManagerCompat.from(context).isHardwareDetected
+
+private fun isFingerprintAvailable(context: Context) = FingerprintManagerCompat.from(context).hasEnrolledFingerprints()
--- app/src/main/java/com/biachacon/sensorsdemo/MainActivity.kt
+++ app/src/main/java/com/biachacon/sensorsdemo/MainActivity.kt
@@ -3,13 +3,58 @@ package com.biachacon.sensorsdemo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.biometric.BiometricPrompt
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var executor: Executor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        executor = Executors.newSingleThreadExecutor()
+
+        val biometricPrompt = BiometricPrompt(
+            this,
+            executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON) {
+                        setTextInfo(text = "Biometric CANCELLED")
+                    } else {
+                        setTextInfo(text = "Biometric ERROR")
+                    }
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    setTextInfo(text = "Biometric OK")
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    setTextInfo(text = "Biometric ERROR")
+                }
+            })
+
+        biometric.setOnClickListener {
+            if (isBiometricAvailable(applicationContext)) {
+                val promptInfo = BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Biometric Authentication")
+                    .setSubtitle("")
+                    .setDescription("Place your finger on the sensor")
+                    .setNegativeButtonText("Cancel")
+                    .build()
+                biometricPrompt.authenticate(promptInfo)
+            } else {
+                setTextInfo(text = "Hardware not supported")
+            }
+        }
 
         proximity.setOnClickListener {
             var i = Intent(this, ProximityActivity::class.java)
@@ -31,6 +76,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(i)
         }
 
+    }
+
+    private fun setTextInfo(text: String) {
+        runOnUiThread {
+
+        }
     }
 
 }
